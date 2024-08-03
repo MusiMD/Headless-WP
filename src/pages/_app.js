a678a1149626f1f354e674315fa006a0d4bfe23f
Devi <devisureshkumar123@gmail.com>
2024-08-03 16:04:04 +0530

    font family updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,22 @@
 import "@/styles/globals.css";
 import Header from '../components/Header'
 import Footer from "@/components/Footer";
+import {Poppins} from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }) {
   return (
   <>
-  <Header/><Component {...pageProps} /><Footer/>
+    <div className={poppins.className}>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
   </>
   );
 }
