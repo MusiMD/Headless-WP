a5ebb781d9c933d5af30031d4ecc1808afd27ffe
Devi <devisureshkumar123@gmail.com>
2024-09-25 12:55:08 +0530

    layout fix completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import Header from '@/components/Header';
 import Footer from "@/components/Footer";
-import { Poppins } from "next/font/google";
+import { Poppins } from "@next/font/google";
 import { getLogo } from '@/lib/pages'; 
 
 const poppins = Poppins({
